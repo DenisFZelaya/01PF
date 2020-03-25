b33eff7ca836b87abb02a969ab26b2e879c8d7f7
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2020-03-25 09:59:48 -0600

    Añadiendo nuevos archivos
    Añadiento nuevos elementos que no se habian subido
    pero que son necesarios para el proyecto

--- src/CajeroNVenta.java
+++ src/CajeroNVenta.java
@@ -6,7 +6,7 @@
 
 /**
  *
- * @author Laptop
+ * //Prueba de cambio para Github
  */
 public class CajeroNVenta extends javax.swing.JFrame {
 
@@ -64,7 +64,7 @@ public class CajeroNVenta extends javax.swing.JFrame {
         panelIMGCajero1Layout.setHorizontalGroup(
             panelIMGCajero1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelIMGCajero1Layout.createSequentialGroup()
-                .addContainerGap(493, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(338, 338, 338))
         );
